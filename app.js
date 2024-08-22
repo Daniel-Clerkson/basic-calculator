64e150e426dc42d0b58cd8f0456b9142b3e0f8b9
Daniel-Clerkson <132950305+Daniel-Clerkson@users.noreply.github.com>
2024-08-22 14:52:30 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let screen = document.getElementById("screen");
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let signs = ["+", "-", "/", "*"];
+let data = {
+     one: "1",
+     two: "2",
+     three: "3",
+     four: "4",
+     five: "5",
+     six: "6",
+     seven: "7",
+     eight: "8",
+     nine: "9",
+     zero: "0",
+     add: "+",
+     sub: "-",
+     divide: "/",
+     mul: "*"
+}
+let value =  document.querSelec
